26559d42a708c40d61f266c97b4c0afa1c25c7c5
prime <74303339+primesx@users.noreply.github.com>
2020-11-30 11:44:11 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,27 @@
+Config = {} -- Burayı Ellemeyin
+Config.primecooldownPolice 	  = 700  -- Polislerin Silahı Çıkarma Süresi
+Config.primecooldown		  = 1700 -- Silahın Çıkarılma Süresi
+
+-- Animasyonlu Çıkarmak İstediğiniz Silahları Buraya Ekleyebilirsiniz.
+Config.primeweapons = {
+	"WEAPON_PISTOL",
+	"WEAPON_COMBATPISTOL",
+	"WEAPON_APPISTOL",
+	"WEAPON_PISTOL50",
+	"WEAPON_SNSPISTOL",
+	"WEAPON_HEAVYPISTOL",
+	"WEAPON_VINTAGEPISTOL",
+	"WEAPON_MARKSMANPISTOL",
+	"WEAPON_MACHINEPISTOL",
+	"WEAPON_VINTAGEPISTOL",
+	"WEAPON_PISTOL_MK2",
+	"WEAPON_SNSPISTOL_MK2",
+	"WEAPON_FLAREGUN",
+	"WEAPON_STUNGUN",
+	"WEAPON_REVOLVER",
+	"WEAPON_KNIFE",
+	"WEAPON_BAT"
+}
+
+
+-- https://github.com/primesx
